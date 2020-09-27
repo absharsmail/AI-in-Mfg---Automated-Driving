8ed3b12784a5d41589d140e0739fe5a74455c8a2
Abshar Faiz Bin Mustafa <me17b042@smail.iitm.ac.in>
2020-09-27 20:25:38 +0530

    Added necessary assets

---
+++ Automated Driving/Assets/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody rb;
+    public float force = 10f;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(force, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(-force, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(0, 0, force);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(0, 0, -force);
+        }
+    }
+}
