8228d3cac560d535bbec2124392c08e133321f34
V Mohammad Aaftab <aaftaabv@gmail.com>
2020-10-17 18:29:19 +0530

    training

--- Aaftab/balance_data.py
+++ Aaftab/balance_data.py
@@ -1,41 +1,63 @@
-# balance_data.py
+ # balance_data.py
 
 import numpy as np
 import pandas as pd
 from collections import Counter
 from random import shuffle
+import cv2
 
-train_data = np.load('training_data.npy')
+train_data = np.load('C:/Users/Ant Pc/GitHub/AI-in-Mfg---Automated-Driving/Aaftab/training_data.npy',allow_pickle=True)
+trdt=pd.DataFrame(train_data)
+print(trdt.head())
+'''
+print(trdt[0][100][80:475,50:910].reshape((395*860,1)).shape)
+for i in range(len(trdt)):
+    trdt[0][i]=trdt[0][i][80:475,50:910].reshape(395*860)
 
-df = pd.DataFrame(train_data)
-print(df.head())
-print(Counter(df[1].apply(str)))
+'''
+i=0
+i=[]
 
-lefts = []
-rights = []
-forwards = []
+def rectify(c):
+    choice=c
+    o=0
+    if choice==[1,1,0,0]:
+        o=0
 
-shuffle(train_data)
+    elif choice==[1,0,1,0]:
+        o=1
+
+    elif choice==[1,0,0,1]:
+        o=2
+
+    elif choice==[0,1,1,0]:
+        o=3
+
+    elif choice==[0,1,0,1]:
+        o=4
+    elif choice==[0,0,1,1]:
+        o=5
+    elif choice==[1,0,0,0]:
+        o=6
+    elif choice==[0,1,0,0]:
+        o=7
+    elif choice==[0,0,1,0]:
+        o=8
+    elif choice==[0,0,0,1]:
+        o=9
+
+    else:
+        o=o
+    return o
+
+choice=[]
 
 for data in train_data:
-    img = data[0]
-    choice = data[1]
+    choicee=data[1]
+    choice.append(rectify(choicee))
 
-    if choice == [1,0,0]:
-        lefts.append([img,choice])
-    elif choice == [0,1,0]:
-        forwards.append([img,choice])
-    elif choice == [0,0,1]:
-        rights.append([img,choice])
-    else:
-        print('no matches')
+choice=pd.DataFrame(choice)
 
+choice.to_csv('choice.csv')
 
-forwards = forwards[:len(lefts)][:len(rights)]
-lefts = lefts[:len(forwards)]
-rights = rights[:len(forwards)]
-
-final_data = forwards + lefts + rights
-shuffle(final_data)
-
-np.save('training_data.npy', final_data)
+pd.DataFrame(a).to_csv('poice.csv')
